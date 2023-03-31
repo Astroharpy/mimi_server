bc0313b69507b5ba12ee09058f0b41ba2a5e9d12
astro <astroharpy@outlook.com>
2023-03-31 22:20:25 +0200

    ngrok

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
     'http://localhost:8000',
-    'http://192.168.43.159:3000'
+    'http://192.168.43.159:3000',
+    'https://bac9-105-1-174-247.eu.ngrok.io/'
 ];
 
 module.exports = allowedOrigins;
