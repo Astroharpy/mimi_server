f2265e4d6d00949ad16889f12dfa7ea5570f75c8
Bongani Ntshangase <77122273+bookjukies@users.noreply.github.com>
2023-06-04 19:05:42 +0200

    dns done

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'http://evergreenpasture.co.za/',
+    'http://www.evergreenpasture.co.za/',
     'https://evergreenpasture.co.za/',
     'http://192.168.43.159:3000',
     'https://melodic-mousse-1f9038.netlify.app/'
