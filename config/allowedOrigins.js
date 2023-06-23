cb4dfe5a3f99bb43dd6358d80ebb538308027a3f
Bongani Ntshangase <77122273+bookjukies@users.noreply.github.com>
2023-06-23 18:05:55 +0200

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://evergreenpasture.co.za/',
     'https://evergreenpasture.co.za/',
+    'https://6495c18b6e75364f9f9b172e--melodic-mousse-1f9038.netlify.app/',
     'http://192.168.43.159:3000',
     'http://192.168.0.110:3000'
 ];
